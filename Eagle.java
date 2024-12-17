0a0430d4d17af6099f8ed0dad131fd62a9bd604a
ELOISE DUFAU <eloise.dufau@gmail.com>
2024-12-17 17:12:16 +0100

    ok

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,58 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff(){
+        if(!this.flying && this.altitude==0){
+            System.out.println(this.getName() + " take off in the sky");
+            this.flying=true;
+        } else {
+            System.out.println(this.getName() + " is already in the sky");
+        }
+    }
+
+    public int descend(int altitude){
+        if( this.flying && this.altitude>0){
+            this.altitude-=altitude;
+            if (this.altitude<=0){
+                this.altitude=0;
+                land();
+            } else {
+            System.out.println(this.getName() + " flies downward, altitude : " +this.altitude);
+            }
+        }
+        return this.altitude;
+    }
+
+    public void glide(){
+        if(this.flying && this.altitude>150){
+            System.out.println(this.getName() + " glides into the Air");
+        } else {
+            System.out.println(this.getName() + " is on the ground");
+        }
+    }
+
+    public void land(){
+        if(this.altitude>0&&this.altitude<=1){
+            this.flying=false;
+            System.out.println(this.getName() + " lands on the ground");
+        } else {
+            System.out.println(this.getName() + " is too high, it can't land");
+        }
+    }
+
+    public int ascend(int altitude){
+        if( this.flying){
+            this.altitude+=altitude;
+            if (this.altitude>150){
+                glide();
+            }
+            System.out.println(this.getName() + " flies upward, altitude : " +this.altitude);
+        } else {
+            System.out.println(this.getName() + " is not take off");
+        } 
+        return this.altitude;
     }
 }
